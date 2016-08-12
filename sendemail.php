5981234b97d9a4a01a5d77956aec2e958ad636c4
Maria <marianalimong@hotmail.com>
2016-08-12 01:21:38 -0500

    actualizacion

---
+++ sendemail.php
@@ -0,0 +1,16 @@
+<?php
+
+  $name = @trim(stripslashes($_POST['name']));
+  $email = @trim(stripslashes($_POST['email']));
+  $phoneno = @trim(stripslashes($_POST['phoneno']));
+  $message = @trim(stripslashes($_POST['message']));
+
+  $email_from = $email;
+  $email_to = 'marianalimong@hotmail.com';
+  $asunto = 'Contacto desde nuestro sitio web';
+
+    $body = 'Nombre: ' . $name . "\n\n" . 'E-mail: ' . $email . "\n\n" . 'No. Telefonico: ' . $phoneno . "\n\n"  . 'Message: ' . $message;
+    $success = @mail($email_to, $asunto, $body, 'From: <'.$email_from.'>');
+
+
+header("Location: http://http://www.lapazedomex.com.mx/");?>
